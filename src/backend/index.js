019ebff7d1f0b12248ed0290c8bad1f27913a7d2
Zippybonzo <90575488+Zippybonzo@users.noreply.github.com>
2024-05-16 09:01:27 +0000

    for now this is what I've done :)

---
+++ src/backend/index.js
@@ -0,0 +1,1 @@
+const Instance = require('./instances')
---
+++ src/backend/instances.js
@@ -0,0 +1,29 @@
+class Instance {
+    constructor(instancename, instancetype, instancestate) {
+        this.instancename = instancename;
+        this.instancetype = instancetype;
+        this.instancestate = instancestate;
+    }
+
+    startInstance() {
+        // code to start instance
+    }
+
+    restartInstance() {
+        // code to restart instance
+    }
+
+    instanceStatus() {
+        // code to get instance status
+    }
+
+    stopInstance() {
+        // code to stop instance
+    }
+
+    removeInstance() {
+        // code to remove instance
+    }
+}
+
+module.exports =  Instance;
